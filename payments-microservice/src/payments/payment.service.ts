3cb02a6be40ad6b8f9597d001768e01344536f18
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-15 02:25:39 +0200

    payment services finished

--- payments-microservice/src/payments/payment.service.ts
+++ payments-microservice/src/payments/payment.service.ts
@@ -1,17 +1,33 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { CreatePaymentDto } from './dtos/CreatePayments.dto';
 import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { User } from 'src/typeorm/User';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Payment } from 'src/typeorm/Payment';
 
 @Injectable()
 export class PaymentsService {
-  constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {}
+  constructor(
+    @InjectRepository(Payment) private paymentsRepository: Repository<Payment>,
+    @Inject('NATS_SERVICE') private natsClient: ClientProxy,
+  ) {}
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async createPayment({ userId, ...createPaymentDto }: CreatePaymentDto) {
-    //TODO :
-    /*     const user = await lastValueFrom(
+    const user = await lastValueFrom<User>(
       this.natsClient.send({ cmd: 'getUserById' }, { userId }),
-    ); */
+    );
+    console.log(user);
+    if (user) {
+      const newPayment = this.paymentsRepository.create({
+        ...createPaymentDto,
+        user,
+      });
+      console.log(newPayment);
+      return this.paymentsRepository.save(newPayment);
+    }
     return null;
   }
 }
--- payments-microservice/src/payments/payments.module.ts
+++ payments-microservice/src/payments/payments.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PaymentsMicroserviceController } from './payments.controller';
 import { NatsClientModule } from '../nats-client/nats-client.module';
 import { PaymentsService } from './payment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from 'src/typeorm/Payment';
+import { User } from 'src/typeorm/User';
 
 @Module({
-  imports: [NatsClientModule],
+  imports: [TypeOrmModule.forFeature([Payment, User]), NatsClientModule],
   controllers: [PaymentsMicroserviceController],
   providers: [PaymentsService],
 })
