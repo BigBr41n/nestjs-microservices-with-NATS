4ce80eb7de92a87dc2b86a5df5bdb8f61a940b7f
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-15 02:24:42 +0200

    adding typeorm to users

--- users-microservice/src/users/users.controller.ts
+++ users-microservice/src/users/users.controller.ts
@@ -1,12 +1,24 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateUserDto } from './dtos/CreateUser.dto';
+import { UsersService } from './users.service';
 
 @Controller()
 export class UsersMicroserviceController {
+  constructor(private usersService: UsersService) {}
   @MessagePattern({ cmd: 'createUser' })
   createUser(@Payload() data: CreateUserDto) {
     console.log('createUser', data);
     return { message: 'sa7it a l7adj' };
+  }
+  @MessagePattern({ cmd: 'getUserById' })
+  getUserById(@Payload() data) {
+    const { userId } = data;
+    return this.usersService.getUserById(userId);
+  }
+
+  @EventPattern('paymentCreated')
+  paymentCreated(@Payload() data: any) {
+    console.log(data);
   }
 }
--- users-microservice/src/users/users.module.ts
+++ users-microservice/src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersMicroserviceController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/User';
+import { Payment } from 'src/typeorm/Payment';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Payment])],
   controllers: [UsersMicroserviceController],
-  providers: [],
+  providers: [UsersService],
 })
 export class UsersModule {}
