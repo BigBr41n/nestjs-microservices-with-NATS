6967a2b10b624a8dff518075d0636669eff5772a
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-15 02:25:16 +0200

    users done

--- users-microservice/src/app.module.ts
+++ users-microservice/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './typeorm/Payment';
+import { User } from './typeorm/User';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'nestjs_db',
+      entities: [User, Payment],
+      synchronize: true,
+      username: 'testuser',
+      password: 'testuser123',
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
